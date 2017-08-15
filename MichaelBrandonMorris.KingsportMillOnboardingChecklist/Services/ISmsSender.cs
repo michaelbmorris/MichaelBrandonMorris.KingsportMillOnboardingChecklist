5f8d1a1baed5611e586f9f0045fa4c37106670ee
Michael Brandon Morris <brandon@michaelbmorris.com>
2017-08-15 07:52:14 -0400

    Add project files.

---
+++ MichaelBrandonMorris.KingsportMillOnboardingChecklist/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MichaelBrandonMorris.KingsportMillOnboardingChecklist.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
